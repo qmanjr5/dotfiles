34a1b96177bb6f4d5cf084dff92ae934a6751e22
Quentin Boughner <Quentin@quentinboughner.com>
2014-07-24 16:00:58 +0000

    Added bash setup script

---
+++ bash/scripts/setup.sh
@@ -0,0 +1,13 @@
+#Dotfiles startup script
+
+#Symlink .sh_history, .bash_history, and .mysql_history to /dev/null
+#This prevents passwords being written to these files (and other information you may not want there)
+#It also still allows command scrollback for the current bash session
+for i in $HOME/.sh_history $HOME/.bash_history $HOME/.mysql_history
+do
+    if [ -f $i ];
+    then
+        rm -f $i;
+    fi
+    ln -s /dev/null $i
+done
